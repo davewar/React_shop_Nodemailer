a44a653b0bdc4a7516949a5baa794ed55ff1fff4
dave <davewarwicker1@gmail.com>
2021-03-25 15:48:04 +0000

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Home = () => {
+
+        return (
+        <div className="container">
+            <h1 className="text-center mt-5 " >Home page</h1>
+            
+        </div>
+    )
+}
+
+export default Home
